b37c2b1976f53b45f06435ee80873d779349357d
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-13 10:39:25 +0100

    add and delete contact done

--- main.js
+++ main.js
@@ -15,3 +15,39 @@ let annuaire = [
         age: 17
     }
 ];
+
+let getContacts = () => {
+
+}
+
+let getChoice =
+    prompt(`Menu
+    1) Voir tous les contacts
+    2) Ajouter un contact
+    3) Supprimer un contact
+    4) Quitter 
+    `)
+
+switch(getChoice) {
+    case "1" :
+        for(let contact of annuaire) {
+             console.log(`Voici la liste de tous mes contacts : ${contact.nom} ${contact.prenom} ${contact.age}`)
+        }
+        break;
+    case "2" :
+        let nom = prompt("Veuillez saisir votre nom de famille :");
+        let prenom = prompt("Veuillez saisir votre prénom :");
+        let age = Number(prompt("Veuillez saisir l'âge :"));
+        let newContact = {nom, prenom, age};
+        annuaire.push(newContact)
+        console.table(annuaire)
+        break;
+    case "3" :
+        let deleteChoice = Number(prompt("Quel contact souhaitez-vous supprimer :"));
+        annuaire.splice(deleteChoice,1);
+        console.table(annuaire)
+        break;
+    case "4" :
+        alert("Au revoir, merci de votre visite.")
+        break;
+}
