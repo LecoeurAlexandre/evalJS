3b7c46b2f18ff5bf1cd58abc6a1a86a9be7502b1
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-13 11:57:50 +0100

    last commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let annuaire = [
 let getContacts = () => {
 
 }
-
+/*
 let getChoice =
     prompt(`Menu
     1) Voir tous les contacts
@@ -27,11 +27,23 @@ let getChoice =
     3) Supprimer un contact
     4) Quitter 
     `)
+*/
+const getChoice = function () {
+    let choice = prompt(`Menu
+    1) Voir tous les contacts
+    2) Ajouter un contact
+    3) Supprimer un contact
+    4) Quitter 
+    `)
+    console.log(choice)
+    return choice
+}
 
-switch(getChoice) {
+
+switch(getChoice()) {
     case "1" :
         for(let contact of annuaire) {
-             console.log(`Voici la liste de tous mes contacts : ${contact.nom} ${contact.prenom} ${contact.age}`)
+             alert(`Voici la liste de tous mes contacts : ${contact.nom} ${contact.prenom} ${contact.age} ans`);
         }
         break;
     case "2" :
@@ -39,15 +51,15 @@ switch(getChoice) {
         let prenom = prompt("Veuillez saisir votre prénom :");
         let age = Number(prompt("Veuillez saisir l'âge :"));
         let newContact = {nom, prenom, age};
-        annuaire.push(newContact)
-        console.table(annuaire)
+        annuaire.push(newContact);
+        console.table(annuaire);
         break;
     case "3" :
         let deleteChoice = Number(prompt("Quel contact souhaitez-vous supprimer :"));
         annuaire.splice(deleteChoice,1);
-        console.table(annuaire)
+        console.table(annuaire);
         break;
     case "4" :
-        alert("Au revoir, merci de votre visite.")
+        alert("Au revoir, merci de votre visite.");
         break;
 }
